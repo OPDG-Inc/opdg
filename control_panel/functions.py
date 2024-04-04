c23166015e5bad59a8513bf339b3c1cc1fd99c8c
lrrrtm <lrrrtm.edu@yandex.ru>
2024-04-04 11:11:09 +0300

    added functions.py

---
+++ control_panel/functions.py
@@ -0,0 +1,22 @@
+from json import dump, load
+import os
+
+
+def update_config_file(data, file):
+    with open(file=file, mode="w", encoding="utf-8") as config_file:
+        dump(data, config_file, indent=2)
+
+
+def load_config_file(file):
+    with open(file=file, mode="r", encoding="utf-8") as config_file:
+        data = load(config_file)
+    return data
+
+
+def get_project_folder():
+    script_path = os.path.abspath(__file__)
+    script_directory = os.path.dirname(script_path)
+    os.chdir(script_directory)
+    project_folder = os.getcwd()
+
+    return project_folder

5b14a8e6731485146cf2881c1ad2d73c11754a61
lrrrtm <lrrrtm.edu@yandex.ru>
2024-04-04 17:20:28 +0300

    bug fixes

--- control_panel/app.py
+++ control_panel/app.py
@@ -428,7 +428,7 @@ def main(page: ft.Page):
         change_screen('error')
     else:
         change_screen("login")
-    close_dialog(loading_dialog)
+    page.dialog = None
     page.update()
 
 

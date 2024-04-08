996387f29e2f0e63d3031aa1c620312a64cb94fe
lrrrtm <lrrrtm.edu@yandex.ru>
2024-04-08 07:27:51 +0300

    bug fixes

--- control_panel/flask_yandex.py
+++ control_panel/flask_yandex.py
@@ -5,10 +5,18 @@ from flask import Flask, request, Response
 from mysql.connector import connect
 from dotenv import load_dotenv
 from functions import load_config_file
-from app import create_db_connection
 
 app = Flask(__name__)
 load_dotenv()
+
+connection = connect(
+    host=os.getenv('DB_HOST'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    database=os.getenv('DB_NAME')
+)
+cur = connection.cursor(dictionary=True)
+connection.autocommit = True
 
 
 def insert_topic(data):
@@ -16,11 +24,8 @@ def insert_topic(data):
     true_access_code = load_config_file('config.json')['upload_topic_access_code']
     topic_list = [(a,) for a in data['Список тем'].split('\n') if a.strip()]
     if user_access_code == true_access_code:
-        connection, cur = create_db_connection()
-        if connection.is_connected():
-            sql_query = "INSERT INTO topic (description) VALUES (%s)"
-            cur.executemany(sql_query, topic_list)
-        connection.close()
+        sql_query = "INSERT INTO topic (description) VALUES (%s)"
+        cur.executemany(sql_query, topic_list)
 
 
 @app.route('/uploadtopic', methods=['POST'])
@@ -34,12 +39,10 @@ def upload_topic():
 @app.route('/check', methods=['GET'])
 def check_status():
     if request.method == 'GET':
-        connection, cur = create_db_connection()
-        if connection is not None:
-            if connection.is_connected():
-                return Response('OK', status=200)
-            else:
-                return Response('CONNECTION_ERR', status=503)
+        if connection.is_connected():
+            return Response('OK', status=200)
+        else:
+            return Response('CONNECTION_ERR', status=503)
 
 
 if __name__ == '__main__':

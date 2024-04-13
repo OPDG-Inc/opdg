fc33827bb7150cf06d3bac6cb049017c936ed7d6
Ivan Simonenko <simonenkoivansimon@gmail.com>
2024-04-14 02:54:37 +0300

    updated handlers (#8.2)

--- src/bot/handlers/upload_video.py
+++ src/bot/handlers/upload_video.py
@@ -1,10 +1,12 @@
-from aiogram import F, Router
+from aiogram import Bot, F, Router
 from aiogram.filters import Command
 from aiogram.types import Message
 
+from src.bot.filters import IsUser
 from src.bot.structures.keyboards import TO_MENU, USER_MAIN_MENU_BOARD
 
 router = Router()
+router.message.filter(IsUser())
 
 
 @router.message(Command(commands=["upload_video"]))
@@ -17,9 +19,21 @@ async def cmd_upload_video(message: Message):
     )
 
 
+@router.message(F.video)
+async def cmd_upload_video(bot: Bot, message: Message):
+    file_id = message.video.file_id
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    await bot.download_file(file_path, "video.mp4")
+    await message.answer(
+        text="Вау, это действительно видео!",
+        reply_markup=USER_MAIN_MENU_BOARD
+    )
+
+
 @router.message(F.text == "На главное меню")
 async def cmd_to_main_menu(message: Message):
     await message.answer(
-        text="Главное меню.",
+        text="Главное меню",
         reply_markup=USER_MAIN_MENU_BOARD
     )
